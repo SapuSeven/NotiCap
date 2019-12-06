173cbb7b67f73faabe42a54d94a01ad5c2d5bd3c
Philipp Kreuzbichler <ph@kreuzbichler.com>
2019-12-06 11:51:41 +0100

    Migration to androidx

--- app/src/main/java/com/sapuseven/noticap/utils/FilterRule.java
+++ app/src/main/java/com/sapuseven/noticap/utils/FilterRule.java
@@ -1,7 +1,7 @@
 package com.sapuseven.noticap.utils;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import org.json.JSONArray;
 import org.json.JSONException;
